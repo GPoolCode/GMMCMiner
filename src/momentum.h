8f6131f6318030aecbdb1c17bc686b70bd34719c
GPoolCode <info@gpool.net>
2013-12-31 01:12:10 +0800

    Update momentum.h

--- src/momentum.h
+++ src/momentum.h
@@ -1,7 +1,12 @@
-#pragma once
+#ifndef __MOMENTUM__
+#define __MOMENTUM__
 #include "uint256.h"
 #include <vector>
 #include "main.h"
 
+extern unsigned int memorySize;
+
 void SHA512Filler(char *mainMemoryPsuedoRandomData, int threadNumber, int totalThreads, uint256 midHash);
-void aesSearch(char *mainMemoryPsuedoRandomData, int threadNumber, int totalThreads, WorkThread*, bool *quitFlag);
+void aesSearch(char *mainMemoryPsuedoRandomData, int threadNumber, int totalThreads, uint256 midHash, WorkThread*, bool *quitFlag);
+
+#endif
