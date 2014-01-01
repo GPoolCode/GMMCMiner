36621840bb868eeaf031d81cc3d6d1324045e0b7
GPoolCode <info@gpool.net>
2014-01-01 16:31:41 +0800

    print hashmeter & format code

--- src/main.h
+++ src/main.h
@@ -14,66 +14,66 @@ class WorkThread;
 
 class MiningThreadStep1: public QThread {
 public:
-    int threadNumber;
-    int totalThreads;
-    char *mainMemory;
-    uint256 midHash;
-    void run();
+	int threadNumber;
+	int totalThreads;
+	char *mainMemory;
+	uint256 midHash;
+	void run();
 };
 
 class MiningThreadStep2: public QThread {
 public:
-    int threadNumber;
-    int totalThreads;
-    char *mainMemory;
-    uint256 midHash;
-    bool *quitFlag;
-    WorkThread *parentThread;
-    void run();
+	int threadNumber;
+	int totalThreads;
+	char *mainMemory;
+	uint256 midHash;
+	bool *quitFlag;
+	WorkThread *parentThread;
+	void run();
 };
 
 class WorkThread: public QThread {
 public:
-    GPoolClient *client;
-    MiningThreadStep1 workerStep1[MAX_THREADS];
-    MiningThreadStep2 workerStep2[MAX_THREADS];
-    int workerCount;
-    char *mainMemory;
-    bool quitFlag;
-    
-    unsigned int nVersion;
-    uint256 hashPrevBlock;
-    uint256 hashMerkleRoot;
-    unsigned int nTime;
-    unsigned int nBits;
-    unsigned int nHeight;
-    unsigned int nBitsShare;
-    unsigned int nNonce;
-    uint256 midHash;
-    uint256 shareHash;
-    
-    void getMidHash();
-    void getShareHash();
-    uint256 getFullHash(unsigned int, unsigned int);
-    
-    void run();
-    void submit(unsigned int, unsigned int);
+	GPoolClient *client;
+	MiningThreadStep1 workerStep1[MAX_THREADS];
+	MiningThreadStep2 workerStep2[MAX_THREADS];
+	int workerCount;
+	char *mainMemory;
+	bool quitFlag;
+
+	unsigned int nVersion;
+	uint256 hashPrevBlock;
+	uint256 hashMerkleRoot;
+	unsigned int nTime;
+	unsigned int nBits;
+	unsigned int nHeight;
+	unsigned int nBitsShare;
+	unsigned int nNonce;
+	uint256 midHash;
+	uint256 shareHash;
+
+	void getMidHash();
+	void getShareHash();
+	uint256 getFullHash(unsigned int, unsigned int);
+
+	void run();
+	void submit(unsigned int, unsigned int);
 };
 
 class GPoolClient : public QTcpSocket {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    string user;
-    
-    WorkThread workThread;
-    QMutex submitMutex;
+	string user;
 
-    GPoolClient();
-    bool login();
-    void submit(const char *, const int);
+	WorkThread workThread;
+	QMutex submitMutex;
 
-public slots:
-    void onRead();
+	GPoolClient();
+	bool login();
+	void submit(const char *, const int);
+
+	public slots:
+		void onRead();
 };
 
 
